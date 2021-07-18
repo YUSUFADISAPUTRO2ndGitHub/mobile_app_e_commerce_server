c2d93f1bea50275cdd492b3b2f2f67c485ac33fb
YUSUF <75290419+vantsingDevelopment@users.noreply.github.com>
2021-07-18 23:17:50 +0700

    update

--- www/js/product_details.js
+++ www/js/product_details.js
@@ -233,11 +233,7 @@ function fillingInInformations(productNo){
             $("#main-cod").html("Rp. " + commafy(response.Sell_Price * 1) );
             $("#main-period").html("Rp. " + commafy(response.GroupBuy_SellPrice));
             $("#main-period-2").html("Rp. Hidden | Tersembunyi");
-            if(response.stock_quantity != undefined){
-                if((response.stock_quantity * 1) > 0){
-                    $("#main-available-stock").html(response.stock_quantity + "tersisah");
-                }
-            }
+            $("#main-available-stock").html(response.Stock_Quantity + " tersisah");
             $("#main-details").html(response.Description + "<br><br>Specification:" + response.Specification + "<br>Color: " + response.Color);
             $("#product-id").val(response.Product_Code);
             $("#product-option-dropdown-name").val(response.Color);
